e1c7ac092d7baf15d4f4ec2c29bf9842224bdde5
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-08-25 22:10:48 +0200

    fixed test user page

--- mysite/configuration/views.py
+++ mysite/configuration/views.py
@@ -8,30 +8,38 @@ def __init__(self):
     self.count = 0
 
 def handle_request(request):
-    if request.method == "POST"and request.POST['action'] == 'edit_databases':
+    if request.method == 'POST'and request.POST['action'] == 'edit_databases':
         dbs = request.POST['dbs']
 
         all_lines = []
-        file = open("../config", "r")
+        file = open('../config', 'r')
         for line in file:
             line = line.strip()
             all_lines.append(line)
 
-        file = open("../config", "w")
+        file = open('../config', 'w')
         for line in all_lines:
-            if line.startswith("DATABASES:"):
-                file.write("DATABASES:" + dbs + "\n")
+            if line.startswith('DATABASES:'):
+                file.write('DATABASES:' + dbs + '\n')
             else:
-                file.write(line + "\n")
+                file.write(line + '\n')
         file.close()
 
         viewcore.DATABASES = []
         viewcore.database_instance()
 
-    if request.method == "POST"and request.POST['action'] == 'add_kategorie':
+    if request.method == 'POST'and request.POST['action'] == 'add_kategorie':
         viewcore.database_instance().einzelbuchungen.add_kategorie(request.POST['neue_kategorie'])
 
-    context = viewcore.generate_base_context("configuration")
+    context = viewcore.generate_base_context('configuration')
+    default_databases = ''
+    for db in viewcore.DATABASES:
+        if len(default_databases) != 0:
+            default_databases = default_databases + ','
+        default_databases = default_databases + db
+    context['default_databases'] = default_databases
+    return context
+
 def index(request):
     context = handle_request(request)
 
