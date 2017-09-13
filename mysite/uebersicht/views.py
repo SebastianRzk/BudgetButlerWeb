11bf1f05ce116139cba837892e3c8d348be4dd22
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-09-13 23:23:33 +0200

    switched from set to dict

--- mysite/uebersicht/views.py
+++ mysite/uebersicht/views.py
@@ -28,7 +28,15 @@ def handle_request(request):
         link = "addeinnahme"
         if row.Wert < 0:
             link = "addeinzelbuchung"
-        ausgaben_liste.append((row_index, row.Datum, row.Name, row.Kategorie, '%.2f' % row.Wert, row.Dynamisch, link, str(row.Tags)))
+        ausgaben_liste.append({
+            'index':row_index,
+            'datum':row.Datum,
+            'name':row.Name,
+            'kategorie':row.Kategorie,
+            'wert':'%.2f' % row.Wert,
+            'dynamisch':row.Dynamisch,
+            'link':link,
+            'tags':str(row.Tags)})
 
     if datum_alt != None:
         ausgaben_monatlich["" + str(datum_alt.year) + "." + str(datum_alt.month)] = ausgaben_liste
