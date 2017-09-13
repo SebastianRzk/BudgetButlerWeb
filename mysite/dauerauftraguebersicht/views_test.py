2fc672930e11a1b9efddbe7e0124511cc6222051
RosesTheN00b <sebastian.ruziczka@gmail.com>
2017-09-13 23:02:51 +0200

    added test for dauerauftragsdeletion

--- mysite/dauerauftraguebersicht/views_test.py
+++ mysite/dauerauftraguebersicht/views_test.py
@@ -2,30 +2,36 @@ import os
 import sys
 import unittest
 
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + "/../")
-
+from adddauerauftrag.view_test import PostRequest, GetRequest
 from core.DatabaseModule import Database
 from dauerauftraguebersicht import views
 import viewcore
+from viewcore.converter import datum
+
+
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + "/../")
 
 
 
-
-# Create your tests here.
 class Dauerauftragsuebersicht(unittest.TestCase):
 
-    def setUp(self):
-        print("create new database")
-        self.testdb = Database("test")
-        viewcore.viewcore.DATABASE_INSTANCE = self.testdb
+    def set_up(self):
+        viewcore.viewcore.DATABASE_INSTANCE = Database("test")
         viewcore.viewcore.DATABASES = ['test']
         viewcore.viewcore.TEST = True
 
     def test_init(self):
-        self.setUp()
+        self.set_up()
         result = views.handle_request(GetRequest())
 
+    def test_delete(self):
+        self.set_up()
+        dauerauftraege = viewcore.viewcore.database_instance().dauerauftraege
+        dauerauftraege.add(datum('01/01/2011'), datum('01/01/2011'), '', '11', 'monatlich', 1)
+        dauerauftraege.add(datum('01/01/2011'), datum('01/01/2011'), '', '22', 'monatlich', 1)
 
-class GetRequest():
-    method = "GET"
+        result = views.handle_request(PostRequest({'action':'delete', 'delete_index':'1'}))
+
+        assert len(dauerauftraege.content) == 1
+        assert dauerauftraege.content.Name.tolist() == ['11']
