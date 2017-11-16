41b8461c1e21fcd96bbc9eabe413e85d58535dbe
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2017-11-16 12:20:28 +0100

    added selenium test example

--- mysite/production/views.py
+++ mysite/production/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import redirect
 from viewcore import viewcore
-
+from test import DBManagerStub
 # Create your views here.
 def leave_debug(request):
+    print(request.GET.url)
     viewcore.switch_database_instance(request.GET['database'])
-    return redirect("/dashboard/")
+    return redirect('/dashboard/')
+
+def enter_testmode(request):
+    DBManagerStub.setup_db_for_test()
+    print('WARNUNG: ENTERING TESTMODE')
+    return redirect('/dashboard/')
+
