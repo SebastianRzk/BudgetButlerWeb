a2c15c0a4b32fe429a47e4d675f4734dd7c12163
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2017-11-22 18:34:11 +0100

    replaced maureen with name of partner

--- mysite/addgemeinsam/views.py
+++ mysite/addgemeinsam/views.py
@@ -74,7 +74,7 @@ def handle_request(request):
         context['approve_title'] = 'Gemeinsame Ausgabe aktualisieren'
 
     context['ID'] = viewcore.get_next_transaction_id()
-    context['personen'] = [viewcore.database_instance().name, 'Maureen']
+    context['personen'] = [viewcore.database_instance().name, viewcore.name_of_partner()]
     context['kategorien'] = sorted(viewcore.database_instance().einzelbuchungen.get_kategorien_ausgaben())
     context['letzte_erfassung'] = reversed(viewcore.get_changed_gemeinsamebuchungen())
     return context
