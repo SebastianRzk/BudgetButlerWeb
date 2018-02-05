20ca47eb8abe62fb9c9a7dc16ff0f918f8d09bf8
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-02-05 18:09:54 +0100

    implemented report generator

--- mysite/core/ReportGenerator.py
+++ mysite/core/ReportGenerator.py
@@ -3,28 +3,33 @@ from viewcore.converter import from_double_to_german
 
 class ReportGenerator():
 
-    _half_lines = []
-    _full_lines = []
 
 
     def __init__(self):
         self._col_width = 38
+        self._full_lines = []
+        self._half_lines = []
 
     def add_half_line_elements(self, summary_data):
         for element in summary_data:
             self._half_lines.append(element.ljust(self._col_width, ' '))
 
             for data_element in summary_data[element]:
-                name = ('   ' + data_element).ljust(27, ' ')
+                name = ('   ' + self._shorten(data_element, 25)).ljust(28, ' ')
                 value = summary_data[element][data_element]
 
                 vorzeichen = '+'
                 if value < 0:
                     vorzeichen = ''
 
-                value = (vorzeichen + from_double_to_german(summary_data[element][data_element])).rjust(11, ' ')
+                value = (vorzeichen + from_double_to_german(summary_data[element][data_element])).rjust(10, ' ')
 
                 self._half_lines.append(name+value)
+
+    def _shorten(self, name, max_len):
+        if len(name) > max_len:
+            name = name[:(max_len-3)] + '...'
+        return name
 
     def get_raw_half_lines(self):
         return self._half_lines
--- mysite/core/ReportGenerator_test.py
+++ mysite/core/ReportGenerator_test.py
@@ -18,4 +18,40 @@ class ReportGenerator_test(unittest.TestCase):
                                                   '   Miete                       +123,45',
                                                   '   Sonstiges                    -34,12']
 
-            
+    def test_add_halfline_element_should_shorten_to_long_names(self):
+        generator = ReportGenerator()
+
+        data = {
+                'Einnahmen': {'MieteMieteMieteMiete1234567890': 123.45}
+            }
+        generator.add_half_line_elements(data)
+        for l in generator.get_raw_half_lines():
+            print(l+"|")
+        assert generator.get_raw_half_lines() == ['Einnahmen                             ',
+                                                  '   MieteMieteMieteMiete12...   +123,45']
+
+
+    def test_add_halfline_element_max_range(self):
+        generator = ReportGenerator()
+
+        data = {
+                'Einnahmen': {'MieteMieteMieteMiete1234567890': 12345.45}
+            }
+        generator.add_half_line_elements(data)
+        for l in generator.get_raw_half_lines():
+            print(l+"|")
+        assert generator.get_raw_half_lines() == ['Einnahmen                             ',
+                                                  '   MieteMieteMieteMiete12... +12345,45']
+
+    def test_add_halfline_element_min_range(self):
+        generator = ReportGenerator()
+
+        data = {
+                'Einnahmen': {'A': 0}
+            }
+        generator.add_half_line_elements(data)
+        for l in generator.get_raw_half_lines():
+            print(l+"|")
+        assert generator.get_raw_half_lines() == ['Einnahmen                             ',
+                                                  '   A                             +0,00']
+
