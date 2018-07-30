68bf67e3c4d839c0e496ea4e9a55f8d2b0b46f9b
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-07-30 21:59:35 +0200

    extraced delete to new task

--- online/deleteitems.php
+++ online/deleteitems.php
@@ -4,22 +4,13 @@ try {
 	if( isset($_POST['email']) and isset($_POST['password'])){
 		$auth = getAuth();
 		$auth->login($_POST['email'], $_POST['password']);
-		echo "well done!\r\n\r\n";
 		$dbh = getPDO();
 
-		$sql = "SELECT * FROM `eintraege` WHERE person = :person";
+
+		$sql = "DELETE FROM `eintraege` WHERE person = :person";
 		$sth = $dbh->prepare($sql);
 		$sth->execute(array(':person' => $auth->getUsername()));
-		$alldata = $sth->fetchAll();
 
-
-		$result = "#######MaschinenimportStart\r\nDatum,Kategorie,Name,Wert,Dynamisch";
-		foreach($alldata as $item) {
-			$row = $item['datum'].','.str_replace(',',' ',$item['kategorie']).','.str_replace(',',' ',$item['name']).','.str_replace(',','.',$item['wert']).',False';
-			$result = $result."\r\n".$row;
-		}
-		$result = $result."\n#######MaschinenimportEnd\r\n\r\n";
-		echo $result;
 	}
 }
 catch (\Delight\Auth\InvalidEmailException $e) {
