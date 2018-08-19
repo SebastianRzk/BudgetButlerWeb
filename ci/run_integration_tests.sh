6a6a6abb2be813c10401ccd79f08d216812e2958
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-08-19 17:27:24 +0200

    install geckodriver

--- ci/run_integration_tests.sh
+++ ci/run_integration_tests.sh
@@ -1,4 +1,11 @@
 set -e
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+export PATH=$PATH:$(pwd)/geckodriver
+
+
 cd mysite && python manage.py runserver > build.log &
 
 cd selenium_tests
