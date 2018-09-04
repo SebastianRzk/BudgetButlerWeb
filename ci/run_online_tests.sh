740aacfba34ee2d2a8902b9e2362b46d8c1c880c
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-09-04 10:58:14 +0200

    removed unused delete

--- ci/run_online_tests.sh
+++ ci/run_online_tests.sh
@@ -10,7 +10,7 @@ python create_virtualhost.py
 sudo cp budget.online.conf /etc/apache2/sites-available/
 
 sudo a2ensite budget.online.conf
-sudo rm /etc/apache2/apache.conf 
+#sudo rm /etc/apache2/apache.conf 
 sudo cp apache.conf /etc/apache2/
 
 sudo service apache2 reload
