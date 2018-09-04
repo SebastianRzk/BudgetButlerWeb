d9517c97c6e030eda494b0535488caa5ec470439
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-09-04 14:26:26 +0200

    added ooath in apache conf

--- online_install/create_virtualhost.py
+++ online_install/create_virtualhost.py
@@ -17,16 +17,34 @@ _VIRTUALHOST_FILE_CONTENT = '''
         ServerSignature On
 </VirtualHost>'''
 
+_APACHE_CONF = '''
+<Directory {directory}>
+        Options Indexes FollowSymLinks
+        AllowOverride None
+        Require all granted
+</Directory>'''
+
+
 
 if 'TRAVIS_BUILD_DIR' in os.environ:
     path = os.environ['TRAVIS_BUILD_DIR']
 else:
     path = input('Please enter path of online project: ')
 
-path = path + '/online'
+path = path + '/online/'
 content = _VIRTUALHOST_FILE_CONTENT.format(directory=path)
 print('content:')
 print(content)
+
+
 virtualhost_file = open('budget.online.conf', 'w')
 virtualhost_file.write(content)
 virtualhost_file.close()
+
+
+content = _APACHE_CONF.format(directory=path)
+print('apache conf:')
+print(content)
+apache_conf_file = open('apache.conf')
+apache_conf_file.write(content)
+apache_conf_file.close()
