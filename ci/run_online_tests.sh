289927da1068de368fa4388bb0509e3e38ae1ede
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-09-04 11:10:18 +0200

    added output

--- ci/run_online_tests.sh
+++ ci/run_online_tests.sh
@@ -6,22 +6,28 @@ cd online_install
 sudo apt update
 sudo apt install apache2
 
+echo "Create virtualhost file"
 python create_virtualhost.py
+echo "move virtualhost file"
 sudo cp budget.online.conf /etc/apache2/sites-available/
+echo "installed confs:"
+ls -l  /etc/apache2/sites_available/
 
+echo "Enable site"
 sudo a2ensite budget.online.conf
-#sudo rm /etc/apache2/apache.conf 
+echo "Copy apache configuration"
 sudo cp apache.conf /etc/apache2/
 
+echo "reload apache"
 sudo service apache2 reload
 
+echo "install database"
 pip install -r requirements.txt
 
 python install_database.py
-
-
 cd ..
-cp -r online/* ./
+echo "database installed"
+
 curl 'localhost/login.php'
 echo "DONE"
 
