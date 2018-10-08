0b61d13bb267708af9b008a6e849e407e1324409
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-10-08 20:18:48 +0200

    added api for delete gemeinsam

---
+++ online/deletegemeinsam.php
@@ -0,0 +1,70 @@
+<?php
+require_once('creds.php');
+try {
+	if( isset($_POST['email']) and isset($_POST['password'])){
+		$auth = getAuth();
+		$auth->login($_POST['email'], $_POST['password']);
+		$dbh = getPDO();
+
+
+		$sql = "DELETE FROM `eintraege` WHERE person = :person";
+		$sth = $dbh->prepare($sql);
+		$sth->execute(array(':person' => $auth->getUsername()));
+
+		// Test, ob Verknüpfung mit anderer Person gegeben
+		$other_person_selected = false;
+		$other_person_confirmed = false;
+		$other_name = 'undef';
+
+		$sql = 'select DestinationPerson from gemeinsam_zuordnung where SourcePerson = :sourceperson';
+		$sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute(array(':sourceperson' => $auth->getUsername()));
+		$other = $sth->fetchAll();
+
+		if (sizeof($other) > 0){
+			$other_person_selected = true;
+			$other_name = array_values($other)[0]['DestinationPerson'];
+
+			$sql = 'select DestinationPerson from gemeinsam_zuordnung where SourcePerson = :sourceperson';
+			$sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+			$sth->execute(array(':sourceperson' => $other_name));
+			$sourceperson = $sth->fetchAll();
+			if (sizeof($sourceperson) > 0) {
+				$sourceperson_name = array_values($sourceperson)[0]['DestinationPerson'];
+				if (strcmp($sourceperson_name, $auth->getUsername()) == 0){
+					$other_person_confirmed = true;
+				}
+			}
+		}
+		echo '\n\n os:';
+		echo $other_person_selected;
+		echo '\n\n oc:';
+		echo $other_person_confirmed;
+		echo '\n\n';
+		if ($other_person_confirmed){
+			$sql = "DELETE FROM `eintraege` WHERE person = :person";
+			$sth = $dbh->prepare($sql);
+			$sth->execute(array(':person' => $other_name));
+		}
+
+
+	}
+}
+catch (\Delight\Auth\InvalidEmailException $e) {
+    // wrong email address
+    echo "wrong email";
+}
+catch (\Delight\Auth\InvalidPasswordException $e) {
+	echo "wrong pass";
+}
+catch (\Delight\Auth\EmailNotVerifiedException $e) {
+    // email not verified
+	echo "email not verified";
+}
+catch (\Delight\Auth\TooManyRequestsException $e) {
+    // too many requests
+    echo "too many requests";
+}
+
+
+?>
