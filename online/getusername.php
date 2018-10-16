f365e0540136b469ecfbd6ab62b27b2842053078
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-10-16 20:57:40 +0200

    fixed username bugs

--- online/getusername.php
+++ online/getusername.php
@@ -1,5 +1,7 @@
 <?php
+require_once('creds.php');
 if( isset($_POST['email']) and isset($_POST['password'])){
 	$auth = getAuth();
 	$auth->login($_POST['email'], $_POST['password']);
-	echo $auth->getUsername()
+	echo $auth->getUsername();
+}
