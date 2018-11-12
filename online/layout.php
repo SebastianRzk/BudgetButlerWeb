f7476763aa86cc713d1123f851f97459df512b80
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-11-12 22:28:00 +0100

    added security header tag

--- online/layout.php
+++ online/layout.php
@@ -2,9 +2,11 @@
 function head($pagetitle){
 	echo '<head>';
 	echo '<link rel="stylesheet" type="text/css" href="style.css">';
+	echo '<meta content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff" http-equiv="Content-Type" />';
 	echo '<title>BudgetButlerWeb - ';
 	echo $pagetitle;
 	echo '</title>';
+	header('X-Frame-Options: DENY');
 	echo '<link rel="shortcut icon" type="image/png" href="/logo.png">';
 	echo '</head>';
 }
