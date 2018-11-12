ecc1c654b4c59691d5cc154d4992b6030e042213
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-11-12 22:38:04 +0100

    added security http tags

--- online/layout.php
+++ online/layout.php
@@ -3,10 +3,12 @@ function head($pagetitle){
 	echo '<head>';
 	echo '<link rel="stylesheet" type="text/css" href="style.css">';
 	echo '<meta content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff" http-equiv="Content-Type" />';
+	echo '<meta name="referer" content="no-referer"/>';
 	echo '<title>BudgetButlerWeb - ';
 	echo $pagetitle;
 	echo '</title>';
 	header('X-Frame-Options: DENY');
+	header("X-XSS-Protection: 1; mode=block")
 	echo '<link rel="shortcut icon" type="image/png" href="/logo.png">';
 	echo '</head>';
 }
