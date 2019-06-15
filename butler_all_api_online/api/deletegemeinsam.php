2e0ad529f5552a14d66389622649535d9809a368
RosesTheN00b <git@sebastianruziczka.de>
2019-06-15 10:32:19 +0200

    added all online api

---
+++ butler_all_api_online/api/deletegemeinsam.php
@@ -0,0 +1,22 @@
+<?php
+require_once(__DIR__.'/creds.php');
+require_once(__DIR__.'/model.php');
+authenticated(function(){
+	$auth = getAuth();
+	$dbh = getPDO();
+	$partnerstatus =  get_partnerstatus($auth, $dbh);
+
+	if ($partnerstatus->confirmed) {
+		$sql = "DELETE FROM `gemeinsamebuchungen` WHERE user = :user OR user = :partner";
+		$sth = $dbh->prepare($sql);
+		$sth->execute(array(':user' => $auth->getUsername(),
+			':partner' => $partnerstatus->partnername ));
+	} else {
+		$sql = "DELETE FROM `gemeinsamebuchungen` WHERE user = :user";
+		$sth = $dbh->prepare($sql);
+		$sth->execute(array(':user' => $auth->getUsername()));
+	}
+
+});
+
+?>
