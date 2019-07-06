2379aa77af0144190c176b03048d78315903475c
RosesTheN00b <git@sebastianruziczka.de>
2019-07-06 10:17:26 +0200

    added composer isntall

--- ci/online.sh
+++ ci/online.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 sudo apt update
-sudo apt install npm chromium-browser composer
+
+sudo apt install curl php-cli php-mbstring git unzip
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+
+sudo apt install npm chromium-browser
 
 
 # Installation
