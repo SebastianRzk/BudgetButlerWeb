29b9564f00229793b53821920f06160f1ca7b4fe
RosesTheN00b <git@sebastianruziczka.de>
2019-07-20 16:21:22 +0200

    added doctrine to gemeinsame buchung

---
+++ butler_all_online_api/api/src/GemeinsameBuchung.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once __DIR__."/dtos/GemeinsameBuchungDto.php";
+/**
+ * @Entity @Table(name="gemeinsamebuchungen")
+ **/
+class GemeinsameBuchung
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+    /** @Column(type="string") **/
+    protected $name;
+    /** @Column(type="string") **/
+    protected $user;
+    /** @Column(type="string") **/
+    protected $zielperson;
+    /** @Column(type="string") **/
+    protected $kategorie;
+    /** @Column(type="date") **/
+    protected $datum;
+    /** @Column(type="decimal") **/
+    protected $wert;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getKategorie()
+    {
+        return $this->kategorie;
+    }
+
+    public function getDatum()
+    {
+        return $this->datum;
+    }
+
+    public function getWert()
+    {
+        return $this->wert;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function getZielperson()
+    {
+        return $this->zielperson;
+    }
+
+    public function asDto() {
+	$dto = new GemeinsameBuchungDto();
+	$dto->id = $this->id;
+	$dto->datum = $this->getDatum()->format('Y-m-d');
+	$dto->name = $this->getName();
+	$dto->kategorie = $this->getKategorie();
+	$dto->wert = $this->getWert();
+	$dto->user = $this->getUser();
+	$dto->zielperson = $this->getZielperson();
+	return $dto;
+    }
+}
