5f8e9320a52a8e36501542fc8ddeb7147d1565ed
RosesTheN00b <git@sebastianruziczka.de>
2019-10-21 11:40:43 +0200

    remove test

--- butler_offline/test/core/ReportGenerator_test.py
+++ butler_offline/test/core/ReportGenerator_test.py
@@ -49,33 +49,6 @@ class ReportGenerator_test(unittest.TestCase):
         assert generator.get_raw_half_lines() == ['Einnahmen                             ',
                                                   '   A                             +0,00']
 
-    def test_generate_page(self):
-        generator = ReportGenerator('SamplePage', 10)
-
-        data = {
-            'Einnahmen': {'Firma1': 123.45,
-                          'Firma2': 34.12,
-                          'Firma3': 222.22},
-            'Ausgaben': {'Essen': -300.40,
-                         'Miete': -450.00,
-                         'Versicherung': -200.00,
-                         'Sport': -50.00}
-        }
-        generator.add_half_line_elements(data)
-
-        assert generator.generate_pages() == [
-            'SamplePage                                                                      ',
-            '--------------------------------------------------------------------------------',
-            'Einnahmen                                    Miete                       -450,00',
-            '   Firma1                      +123,45       Versicherung                -200,00',
-            '   Firma2                       +34,12       Sport                        -50,00',
-            '   Firma3                      +222,22                                          ',
-            'Ausgaben                                                                        ',
-            '   Essen                       -300,40                                          ',
-            '--------------------------------------------------------------------------------',
-            '                                                                       Blatt 1/1',
-            '', '']
-
     def test_generate_multible_pages(self):
         generator = ReportGenerator('SamplePage', 7)
 
