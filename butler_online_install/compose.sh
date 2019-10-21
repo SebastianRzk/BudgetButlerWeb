3f4f1eae6288719394841b8d1fe85a846c7b4994
RosesTheN00b <git@sebastianruziczka.de>
2019-10-21 10:01:31 +0200

    moved *all_online -> online

--- butler_online_install/compose.sh
+++ butler_online_install/compose.sh
@@ -9,27 +9,27 @@ echo "------------------------------------------------"
 echo "------------------------------------------------"
 
 echo "Löschen von alten ergebnissen"
-rm -rfv butler_all_online_distribution
+rm -rfv butler_online_distribution
 
 echo "build php api"
-cd butler_all_online_api/api/
+cd butler_online_api/api/
 composer install
 cd -
 
 echo "build ng frontend"
-cd butler_all_online/budgetbutler
+cd butler_online/budgetbutler
 npm install
 ng build --prod
 cd -
 
 echo "create dist"
-mkdir butler_all_online_distribution
-mkdir butler_all_online_distribution/api
-cp -r butler_all_online_api/api/* butler_all_online_distribution/api
-cp -r butler_all_online/budgetbutler/dist/* butler_all_online_distribution
-cp butler_all_online_distribution/api/robots.txt butler_all_online_distribution/
-cp butler_all_online_install/.htaccess butler_all_online_distribution/
-rm butler_all_online_distribution/3rdpartylicenses.txt
+mkdir butler_online_distribution
+mkdir butler_online_distribution/api
+cp -r butler_online_api/api/* butler_online_distribution/api
+cp -r butler_online/budgetbutler/dist/* butler_online_distribution
+cp butler_online_distribution/api/robots.txt butler_online_distribution/
+cp butler_online_install/.htaccess butler_online_distribution/
+rm butler_online_distribution/3rdpartylicenses.txt
 
 
 echo "------------------------------------------------"
