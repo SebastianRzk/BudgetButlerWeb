eabda202b091ee52ed652c2750d7466e6df8cf41
RosesTheN00b <RosesTheN00b@github>
2019-12-06 00:08:56 +0100

    add chromium app parameter

--- start_butler_offline.sh
+++ start_butler_offline.sh
@@ -5,6 +5,6 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path/butler_offline"
 export FLASK_APP=start_as_flask.py 
 
-eval  "sleep 4s && chromium http://localhost:5000" &
+eval  "sleep 4s && chromium --app=http://localhost:5000" &
 flask run
 
