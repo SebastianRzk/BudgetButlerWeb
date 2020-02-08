c0259fff13fbe6ae266257d8d985d1500fa86f2e
RosesTheN00b <RosesTheN00b@github>
2020-02-08 10:22:44 +0100

    move new user to angular forms

--- butler_online/budgetbutler/src/app/settings/adduser/adduser.component.ts
+++ butler_online/budgetbutler/src/app/settings/adduser/adduser.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { MyErrorStateMatcher } from 'src/app/matcher';
 import { AdminService } from '../../auth/admin.service';
 import { PASSWOERTER_NICHT_GLEICH, PASSWORT_ZU_KURZ } from '../errormessages';
@@ -13,10 +13,12 @@ export class AdduserComponent implements OnInit {
 
   public errorMatcher = new MyErrorStateMatcher();
 
-  public username = new FormControl('', Validators.required);
-  public email = new FormControl('', Validators.required);
-  public passwort = new FormControl('', Validators.required);
-  public passwortWiederholung = new FormControl('', Validators.required);
+  newUserForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    passwort: new FormControl('', Validators.required),
+    passwortWiederholung: new FormControl('', Validators.required)
+  });
 
   public errorMessage = '';
 
@@ -29,12 +31,12 @@ export class AdduserComponent implements OnInit {
   }
 
   computeErrorMesage = () => {
-    if (this.passwort.value.length < 8) {
+    if (this.newUserForm.get('passwort').value.length < 8) {
       this.errorMessage = PASSWORT_ZU_KURZ;
       return;
     }
 
-    if (this.passwort.value !== this.passwortWiederholung.value) {
+    if (this.newUserForm.get('passwort').value !== this.newUserForm.get('passwortWiederholung').value) {
       this.errorMessage = PASSWOERTER_NICHT_GLEICH;
       return;
     }
@@ -42,17 +44,22 @@ export class AdduserComponent implements OnInit {
     this.errorMessage = '';
   }
 
-  addUser() {
+  onNewUserSubmit() {
     this.computeErrorMesage();
 
-    if (this.errorMessage === '' && this.email.value !== '' &&  this.username.value !== '') {
-      this.adminService.addUser(this.username.value, this.email.value, this.passwort.value);
+    if (this.errorMessage !== '') {
+      return;
     }
 
-    this.email.setValue('');
-    this.username.setValue('');
-    this.passwort.setValue('');
-    this.passwortWiederholung.setValue('');
+    if (!this.newUserForm.valid) {
+      return;
+    }
+
+    this.adminService.addUser(this.newUserForm.get('username').value,
+      this.newUserForm.get('email').value,
+      this.newUserForm.get('passwort').value);
+
+    this.newUserForm.reset();
   }
 
 }
