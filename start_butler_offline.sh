fb10f7110300f9268968c4c2aa48281973de42fa
RosesTheN00b <RosesTheN00b@github>
2020-02-09 14:09:21 +0100

    shell script waits until server is up

--- start_butler_offline.sh
+++ start_butler_offline.sh
@@ -4,6 +4,10 @@ set -e
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 VENV_DIR=$parent_path/.venv
+HOST=http://localhost:5000
+
+export FLASK_APP=start_as_flask.py
+
 
 if [[ ! -d $VENV_DIR ]]; then
     echo ""
@@ -39,10 +43,14 @@ echo ""
 
 echo "Starting BudgetButlerWeb local server"
 cd "$parent_path/butler_offline"
-export FLASK_APP=start_as_flask.py 
 
 flask run &
 
-sleep 4s && chromium --app=http://localhost:5000
+until $(curl --output /dev/null --silent --head --fail $HOST); do
+    printf 'Waiting for server to be up...'
+    sleep 1
+done
+
+chromium --app=$HOST
 
 pkill -P $$
