94000ee3a51f0a918891e21112b8ce6257ea5775
RosesTheN00b <RosesTheN00b@github>
2020-11-02 21:02:46 +0100

    rename to self and partner

--- butler_offline/views/gemeinsame_buchungen/gemeinsam_abrechnen.py
+++ butler_offline/views/gemeinsame_buchungen/gemeinsam_abrechnen.py
@@ -39,11 +39,10 @@ def _handle_request(request):
     replay_value_if_defined(context, 'set_other_kategorie', request)
     replay_value_if_defined(context, 'set_other_kategorie_value', request, 'Korrekturbuchung')
 
-
-    select_sebastian = selector.fuer(name_self)
-    select_maureen = selector.fuer(name_partner)
-    ausgabe_sebastian = select_sebastian.sum()
-    ausgabe_maureen = select_maureen.sum()
+    select_self = selector.fuer(name_self)
+    select_partner = selector.fuer(name_partner)
+    ausgabe_self = select_self.sum()
+    ausgabe_partner = select_partner.sum()
     ausgabe_gesamt = selector.sum()
 
     ergebnis = ''
@@ -58,36 +57,36 @@ def _handle_request(request):
         limit_value = int(request.values['set_limit_value'])
 
         if limited_person == name_partner:
-            maureen_soll = abs(ausgabe_gesamt * ((100 - set_verhaeltnis) / 100))
-            if maureen_soll > limit_value:
+            partner_soll = abs(ausgabe_gesamt * ((100 - set_verhaeltnis) / 100))
+            if partner_soll > limit_value:
                 set_verhaeltnis = 100 - abs((limit_value / ausgabe_gesamt) * 100)
                 ergebnis += ergebnis_satz.format(name=name_partner, limit_value = limit_value, verhaeltnis_alt=verhaeltnis_alt, verhaeltnis_neu=set_verhaeltnis)
         else:
-            sebastian_soll = abs(ausgabe_gesamt * (set_verhaeltnis / 100))
-            if sebastian_soll > limit_value:
+            self_soll = abs(ausgabe_gesamt * (set_verhaeltnis / 100))
+            if self_soll > limit_value:
                 set_verhaeltnis = abs((limit_value / ausgabe_gesamt) * 100)
                 ergebnis += ergebnis_satz.format(name=name_self, limit_value = limit_value, verhaeltnis_alt=verhaeltnis_alt, verhaeltnis_neu=set_verhaeltnis)
 
-    sebastian_soll = (ausgabe_gesamt * (set_verhaeltnis / 100))
-    sebastian_dif = sebastian_soll - ausgabe_sebastian
-    maureen_soll = (ausgabe_gesamt * ((100 - set_verhaeltnis) / 100))
-    maureen_dif = maureen_soll - ausgabe_maureen
+    self_soll = (ausgabe_gesamt * (set_verhaeltnis / 100))
+    self_diff = self_soll - ausgabe_self
+    partner_soll = (ausgabe_gesamt * ((100 - set_verhaeltnis) / 100))
+    partner_diff = partner_soll - ausgabe_partner
 
     ergebnis_satz = 'Die gemeinsamen Ausgaben sind ausgeglichen.'
-    if maureen_dif > 0 or sebastian_dif < 0:
-        ergebnis_satz = name_partner + ' bekommt von ' + name_self + ' noch ' + str('%.2f' % maureen_dif) + '€.'
+    if partner_diff > 0 or self_diff < 0:
+        ergebnis_satz = name_partner + ' bekommt von ' + name_self + ' noch ' + str('%.2f' % partner_diff) + '€.'
 
-    if sebastian_dif > 0 or maureen_dif < 0:
-        ergebnis_satz = name_self + ' bekommt von ' + name_partner + ' noch ' + str('%.2f' % sebastian_dif) + '€.'
+    if self_diff > 0 or partner_diff < 0:
+        ergebnis_satz = name_self + ' bekommt von ' + name_partner + ' noch ' + str('%.2f' % self_diff) + '€.'
     ergebnis += ergebnis_satz
 
     context['str_ergebnis'] = ergebnis.replace('<br>', '\n')
-    context['ausgabe_maureen'] = "%.2f" % abs(ausgabe_maureen)
-    context['ausgabe_sebastian'] = "%.2f" % abs(ausgabe_sebastian)
-    context['sebastian_diff'] = "%.2f" % sebastian_dif
-    context['maureen_diff'] = "%.2f" % maureen_dif
-    context['sebastian_soll'] = "%.2f" % abs(sebastian_soll)
-    context['maureen_soll'] = "%.2f" % abs(maureen_soll)
+    context['ausgabe_partner'] = "%.2f" % abs(ausgabe_partner)
+    context['ausgabe_self'] = "%.2f" % abs(ausgabe_self)
+    context['self_diff'] = "%.2f" % self_diff
+    context['partner_diff'] = "%.2f" % partner_diff
+    context['self_soll'] = "%.2f" % abs(self_soll)
+    context['partner_soll'] = "%.2f" % abs(partner_soll)
     context['ausgabe_gesamt'] = "%.2f" % abs(ausgabe_gesamt)
     context['ergebnis'] = ergebnis
     context['myname'] = name_self
