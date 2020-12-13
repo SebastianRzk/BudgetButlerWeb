274afa1bb7ffc07d79971710d5354a4844d55d32
RosesTheN00b <RosesTheN00b@github>
2020-12-13 12:09:36 +0100

    mv docker edition, to have good names created by docker compose

--- butler_online_install/compose.sh
+++ butler_online_install/compose.sh
@@ -8,8 +8,6 @@ echo "~~~~~~~~~~~~~~~~~BudgetButlerWeb~~~~~~~~~~~~~~~~"
 echo "------------------------------------------------"
 echo "------------------------------------------------"
 
-echo "Löschen von alten ergebnissen"
-rm -rfv butler_online_distribution
 
 echo "build php api"
 cd butler_online_api/api/
@@ -34,13 +32,14 @@ rm butler_online_distribution/webspace_edition/3rdpartylicenses.txt
 
 echo "create docker edition"
 mkdir butler_online_distribution/docker_edition
-mkdir butler_online_distribution/docker_edition/src/
-mkdir butler_online_distribution/docker_edition/src/api
+mkdir butler_online_distribution/docker_edition/budget_butler
+mkdir butler_online_distribution/docker_edition/budget_butler/src/
+mkdir butler_online_distribution/docker_edition/budget_butler/src/api
 cp -r butler_online_install/docker_edition/* butler_online_distribution/docker_edition/
-cp -r butler_online_api/api/* butler_online_distribution/docker_edition/src/api
-cp -r butler_online/budgetbutler/dist/* butler_online_distribution/docker_edition/src/
-cp butler_online_distribution/docker_edition/src/api/robots.txt butler_online_distribution/docker_edition/src/
-rm butler_online_distribution/docker_edition/src/3rdpartylicenses.txt
+cp -r butler_online_api/api/* butler_online_distribution/docker_edition/budget_butler/src/api
+cp -r butler_online/budgetbutler/dist/* butler_online_distribution/docker_edition/budget_butler/src/
+cp butler_online_distribution/docker_edition/budget_butler/src/api/robots.txt butler_online_distribution/docker_edition/budget_butler/src/
+rm butler_online_distribution/docker_edition/budget_butler/src/3rdpartylicenses.txt
 
 
 echo "------------------------------------------------"
