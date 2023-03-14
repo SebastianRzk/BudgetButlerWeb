5fd5737a23c01d5ffdfb82e0e32475457893de87
SebastianRzk <github@sebastianruziczka.de>
2023-03-14 22:57:24 +0100

    update test structure

--- butler_offline/test/viewcore/viewcore_test.py
+++ butler_offline/test/viewcore/viewcore_test.py
@@ -1,5 +1,3 @@
-import unittest
-
 import butler_offline.viewcore.menu
 import butler_offline.viewcore.routes
 from butler_offline.viewcore import viewcore
@@ -7,34 +5,38 @@ from butler_offline.test.RequestStubs import GetRequest
 from butler_offline.test.RequestStubs import PostRequest
 
 
+def test_post_action_is_with_get_request_should_return_false():
+    assert not viewcore.post_action_is(GetRequest(), "delete")
 
-class ViewcoreTest(unittest.TestCase):
 
-    def test_post_action_is_with_get_request_should_return_false(self):
-        assert not viewcore.post_action_is(GetRequest(), "delete")
+def test_post_action_is_with_empty_post_request_should_return_false():
+    assert not viewcore.post_action_is(PostRequest({}), "delete")
 
-    def test_post_action_is_with_empty_post_request_should_return_false(self):
-        assert not viewcore.post_action_is(PostRequest({}), "delete")
 
-    def test_post_action_is_with_post_request_and_other_action_should_return_false(self):
-        assert not viewcore.post_action_is(PostRequest({'action': 'add'}), "delete")
+def test_post_action_is_with_post_request_and_other_action_should_return_false():
+    assert not viewcore.post_action_is(PostRequest({'action': 'add'}), "delete")
 
-    def test_post_action_is_with_post_request_and_and_matching_action_should_return_true(self):
-        assert viewcore.post_action_is(PostRequest({'action': 'delete'}), "delete")
 
-    def test_getPostParameterOrDefault_withGetRequest_shouldReturnDefault(self):
-        assert viewcore.get_post_parameter_or_default(GetRequest(), 'test', 'default') == 'default'
+def test_post_action_is_with_post_request_and_and_matching_action_should_return_true():
+    assert viewcore.post_action_is(PostRequest({'action': 'delete'}), "delete")
 
-    def test_getPostParameterOrDefault_withPostRequestAndNoMatchingParameter_shouldReturnDefault(self):
-        assert viewcore.get_post_parameter_or_default(PostRequest({}), 'test', 'default') == 'default'
 
-    def test_getPostParameterOrDefault_withPostRequestAndMatchingParameter_shouldReturnValue(self):
-        request = PostRequest({'test' : 'value'})
-        assert viewcore.get_post_parameter_or_default(request, 'test', 'default') == 'value'
-        
-    def test_def_get_menu_list(self):
-        menu_list = butler_offline.viewcore.menu.get_menu_list()
-        assert 'Persönliche Finanzen' in menu_list
-        assert 'Gemeinsame Finanzen' in menu_list
-        assert 'Sparen' in menu_list
-        assert 'Einstellungen' in menu_list
+def test__get_post_parameter_or_default__with_get_request__should_return_default():
+    assert viewcore.get_post_parameter_or_default(GetRequest(), 'test', 'default') == 'default'
+
+
+def test__get_post_parameter_or_default__with_post_request_and_no_matching_parameter__should_return_default():
+    assert viewcore.get_post_parameter_or_default(PostRequest({}), 'test', 'default') == 'default'
+
+
+def test__get_post_parameter_or_default__with_post_request_and_matching_parameter__should_return_value():
+    request = PostRequest({'test': 'value'})
+    assert viewcore.get_post_parameter_or_default(request, 'test', 'default') == 'value'
+
+
+def test_def_get_menu_list():
+    menu_list = butler_offline.viewcore.menu.get_menu_list()
+    assert 'Persönliche Finanzen' in menu_list
+    assert 'Gemeinsame Finanzen' in menu_list
+    assert 'Sparen' in menu_list
+    assert 'Einstellungen' in menu_list
