3aa77a9343be0408e10bd221ebc50882e6f9bd12
SebastianRzk <github@sebastianruziczka.de>
2023-08-19 16:33:40 +0200

    update angular version

--- butler_online/budgetbutler/src/app/pages/auth/auth.guard.ts
+++ butler_online/budgetbutler/src/app/pages/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 
 import {AuthService} from './auth.service';
 import {map, take} from 'rxjs/operators';
@@ -9,7 +9,7 @@ import {Observable} from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {
   }
 
