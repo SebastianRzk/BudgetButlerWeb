943bdf1a79bb82ad312e6741a528cde1a4ee56a9
SebastianRzk <github@sebastianruziczka.de>
2023-08-20 10:53:02 +0200

    move uebersicht gemeinsam to new tranaction handler

--- butler_offline/test/views/gemeinsame_buchungen/uebersicht_gemeinsam_test.py
+++ butler_offline/test/views/gemeinsame_buchungen/uebersicht_gemeinsam_test.py
@@ -1,58 +1,68 @@
-from butler_offline.test.core.file_system_stub import FileSystemStub
 from butler_offline.test.RequestStubs import GetRequest, VersionedPostRequest
-from butler_offline.test.database_util import untaint_database
 from butler_offline.views.gemeinsame_buchungen import uebersicht_gemeinsam
-from butler_offline.core import file_system
-from butler_offline.viewcore import request_handler
-from butler_offline.viewcore.state import persisted_state
-from butler_offline.viewcore.state.persisted_state import database_instance as db
 from butler_offline.viewcore.converter import datum_from_german as datum
+from butler_offline.core.database.gemeinsamebuchungen import Gemeinsamebuchungen
 
 
-def set_up():
-    file_system.INSTANCE = FileSystemStub()
-    persisted_state.DATABASE_INSTANCE = None
-    request_handler.stub_me()
-
-
-def test_transaction_id_should_be_in_context():
-    set_up()
-    context = uebersicht_gemeinsam.index(GetRequest())
-    assert 'ID' in context
+def test_context_should_be_transactional():
+    context = uebersicht_gemeinsam.handle_request(
+        request=GetRequest(),
+        context=uebersicht_gemeinsam.UebersichtGemeinsamContext(
+            gemeinsamebuchungen=Gemeinsamebuchungen()
+        )
+    )
+    assert context.is_transactional()
 
 
 def test_init():
-    set_up()
-    uebersicht_gemeinsam.index(GetRequest())
+    context = uebersicht_gemeinsam.handle_request(
+        request=GetRequest(),
+        context=uebersicht_gemeinsam.UebersichtGemeinsamContext(
+            gemeinsamebuchungen=Gemeinsamebuchungen()
+        )
+    )
+    assert context.is_ok()
 
 
 def test_delete():
-    set_up()
+    gemeinsamebuchungen = Gemeinsamebuchungen()
 
-    db().gemeinsamebuchungen.add(datum('01.01.2011'), 'kat1', 'name1', 1, 'pers1')
-    db().gemeinsamebuchungen.add(datum('01.01.2012'), 'kat2', 'name2', 1, 'pers2')
-    db().gemeinsamebuchungen.add(datum('01.01.2013'), 'kat3', 'name3', 1, 'pers3')
-    untaint_database(database=db())
+    gemeinsamebuchungen.add(datum('01.01.2011'), 'kat1', 'name1', 1, 'pers1')
+    gemeinsamebuchungen.add(datum('01.01.2012'), 'kat2', 'name2', 1, 'pers2')
+    gemeinsamebuchungen.add(datum('01.01.2013'), 'kat3', 'name3', 1, 'pers3')
 
-    uebersicht_gemeinsam.index(VersionedPostRequest({
-        'action': 'delete',
-        'delete_index': 1
-    }))
+    assert uebersicht_gemeinsam.handle_request(
+        request=VersionedPostRequest({
+            'action': 'delete',
+            'delete_index': 1
+        }),
+        context=uebersicht_gemeinsam.UebersichtGemeinsamContext(
+            gemeinsamebuchungen=gemeinsamebuchungen
+        )
+    ).is_ok()
 
-    result = uebersicht_gemeinsam.index(GetRequest())
+    result = uebersicht_gemeinsam.handle_request(
+        request=GetRequest(),
+        context=uebersicht_gemeinsam.UebersichtGemeinsamContext(
+            gemeinsamebuchungen=gemeinsamebuchungen
+        )
+    )
 
-    assert len(result['ausgaben']) == 2
-    assert result['ausgaben'][0]['Name'] == 'name1'
-    assert result['ausgaben'][1]['Name'] == 'name3'
+    assert result.is_ok()
+    assert len(result.get('ausgaben')) == 2
+    assert result.get('ausgaben')[0]['Name'] == 'name1'
+    assert result.get('ausgaben')[1]['Name'] == 'name3'
 
 
 def test_list():
-    set_up()
+    gemeinsamebuchungen = Gemeinsamebuchungen()
+    gemeinsamebuchungen.add(datum('01.01.2011'), 'kat1', 'name1', 1, 'pers1')
 
-    db().gemeinsamebuchungen.add(datum('01.01.2011'), 'kat1', 'name1', 1, 'pers1')
-    untaint_database(database=db())
-
-    result = uebersicht_gemeinsam.index(GetRequest())
-    print(result['ausgaben'])
-    assert result['ausgaben'] == [
+    result = uebersicht_gemeinsam.handle_request(
+        request=GetRequest(),
+        context=uebersicht_gemeinsam.UebersichtGemeinsamContext(
+            gemeinsamebuchungen=gemeinsamebuchungen
+        )
+    )
+    assert result.get('ausgaben') == [
         {'Datum': '01.01.2011', 'Kategorie': 'kat1', 'Name': 'name1', 'Wert': '1,00', 'Person': 'pers1', 'index': 0}]
--- butler_offline/views/gemeinsame_buchungen/uebersicht_gemeinsam.py
+++ butler_offline/views/gemeinsame_buchungen/uebersicht_gemeinsam.py
@@ -1,22 +1,35 @@
-from butler_offline.viewcore.state.persisted_state import database_instance
 from butler_offline.viewcore.viewcore import post_action_is
 from butler_offline.viewcore import request_handler
 from butler_offline.viewcore.converter import to_descriptive_list
-from butler_offline.viewcore.context import generate_transactional_context, generate_redirect_context
+from butler_offline.core.database.gemeinsamebuchungen import Gemeinsamebuchungen
+from butler_offline.viewcore.context.builder import generate_transactional_page_context, generate_redirect_page_context
 
-def _handle_request(request):
+
+class UebersichtGemeinsamContext:
+    def __init__(self, gemeinsamebuchungen: Gemeinsamebuchungen):
+        self._gemeinsamebuchungen = gemeinsamebuchungen
+
+    def gemeinsamebuchungen(self) -> Gemeinsamebuchungen:
+        return self._gemeinsamebuchungen
+
+
+def handle_request(request, context: UebersichtGemeinsamContext):
     if post_action_is(request, 'delete'):
-        database_instance().gemeinsamebuchungen.delete(int(request.values['delete_index']))
-        return generate_redirect_context('/gemeinsameuebersicht/')
+        context.gemeinsamebuchungen().delete(int(request.values['delete_index']))
+        return generate_redirect_page_context('/gemeinsameuebersicht/')
 
-    context = generate_transactional_context('gemeinsameuebersicht')
-    context['ausgaben'] = to_descriptive_list(
-        database_instance().gemeinsamebuchungen.select().to_list())
+    result_context = generate_transactional_page_context('gemeinsameuebersicht')
+    result_context.add('ausgaben', to_descriptive_list(context.gemeinsamebuchungen().select().to_list()))
 
-    return context
+    return result_context
 
 
-# Create your views here.
 def index(request):
-    return request_handler.handle_request(request, _handle_request, 'gemeinsame_buchungen/uebersicht_gemeinsam.html')
-
+    return request_handler.handle(
+        request=request,
+        handle_function=handle_request,
+        html_base_page='gemeinsame_buchungen/uebersicht_gemeinsam.html',
+        context_creator=lambda db: UebersichtGemeinsamContext(
+            gemeinsamebuchungen=db.gemeinsamebuchungen,
+        )
+    )
