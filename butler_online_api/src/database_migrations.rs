9449b0f7d8cb0804f10edb60560006ecf4d2c69c
SebastianRzk <github@sebastianruziczka.de>
2025-01-10 06:38:04 +0100

    ? -> expect

--- butler_online_api/src/database_migrations.rs
+++ butler_online_api/src/database_migrations.rs
@@ -5,6 +5,8 @@ use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations/");
 
 pub fn run_migrations(connection: &mut MysqlConnection) -> Result<&str, Error> {
-    connection.run_pending_migrations(MIGRATIONS)?;
+    connection
+        .run_pending_migrations(MIGRATIONS)
+        .expect("Failed to run migrations");
     Ok("")
 }
